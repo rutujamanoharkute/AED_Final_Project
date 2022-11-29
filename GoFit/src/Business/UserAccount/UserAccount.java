8be39b0a0abccf2bbf55d27bf4123dbe15a8b523
harshada1709 <bagal.h@northeastern.edu>
2022-11-28 23:28:07 -0500

    Created UserAccount classes

---
+++ GoFit/src/Business/UserAccount/UserAccount.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.UserAccount;
+
+import Business.Employee.Employee;
+
+/**
+ *
+ * @author Harshada
+ */
+public class UserAccount {
+    
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ GoFit/src/Business/UserAccount/UserAccountDirectory.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.UserAccount;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Harshada
+ */
+public class UserAccountDirectory {
+    
+    private ArrayList<UserAccount> userAccountList;
+
+    public ArrayList<UserAccount> getUserAccountList() {
+        return userAccountList;
+    }
+
+    public void setUserAccountList(ArrayList<UserAccount> userAccountList) {
+        this.userAccountList = userAccountList;
+    }
+}
