594325973b76c1a963b1fa17d7aad954698be8cc
harshada1709 <bagal.h@northeastern.edu>
2022-11-29 12:19:52 -0500

    Updates to Enterprise

--- GoFit/src/Business/Enterprise/Enterprise.java
+++ GoFit/src/Business/Enterprise/Enterprise.java
@@ -4,10 +4,50 @@
  */
 package Business.Enterprise;
 
+import Business.Organization.Organization;
+import Business.Organization.OrganizationDirectory;
+
 /**
  *
  * @author Harshada
  */
-public class Enterprise {
+public abstract class Enterprise extends Organization{
+    private EnterpriseType enterpriseType;
+
+    public EnterpriseType getEnterpriseType() {
+        return enterpriseType;
+    }
+
+    public void setEnterpriseType(EnterpriseType enterpriseType) {
+        this.enterpriseType = enterpriseType;
+    }
+
+    public OrganizationDirectory getOrganizationDirectory() {
+        return organizationDirectory;
+    }
+
+    public void setOrganizationDirectory(OrganizationDirectory organizationDirectory) {
+        this.organizationDirectory = organizationDirectory;
+    }
+    private OrganizationDirectory organizationDirectory;
     
+    public enum EnterpriseType{
+        DietUnit("Diet Enterprise"),
+        TrainerUnit("Gym Trainer Enterprise"),
+        DoctorUnit("Doctor Enterprise"),
+        PharmacistUnit("Pharmacist Enterprise");
+        
+        private String value;
+        
+         private EnterpriseType(String value){
+            this.value=value;
+        }
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+}
 }
