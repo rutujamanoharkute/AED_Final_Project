3f1ffcdf85418c4eb985969bd49a7cab6288d6a2
harshada1709 <bagal.h@northeastern.edu>
2022-11-28 23:18:31 -0500

    Created Employee class

---
+++ GoFit/src/Business/Employee/Employee.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Employee;
+
+/**
+ *
+ * @author Harshada
+ */
+public class Employee {
+    
+    private String employeeName;
+    private int empId;
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+    
+}
---
+++ GoFit/src/Business/Employee/EmployeeDirectory.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Employee;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Harshada
+ */
+public class EmployeeDirectory {
+    private ArrayList<Employee> employeeList;
+
+    public ArrayList<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(ArrayList<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+    
+}
