23354d2c5790650c58a3b5d44c2fddd21aeacaae
rutujamanoharkute <kute.r@northeastern.edu>
2022-12-08 01:42:31 -0500

    Updated Customer Directory

--- GoFit/src/Business/Customer/CustomerDirectory.java
+++ GoFit/src/Business/Customer/CustomerDirectory.java
@@ -13,30 +13,50 @@ import java.util.ArrayList;
 public class CustomerDirectory {
     
     private ArrayList<Customer> customerList;
+    
+    public CustomerDirectory() {
+        customerList = new ArrayList();
+    }
+
 
     public ArrayList<Customer> getCustomerList() {
         return customerList;
     }
 
-    public void setCustomerList(ArrayList<Customer> customerList) {
-        this.customerList = customerList;
-    }
+   
     
-       public Customer createCustomer(Customer customer){
-        //Customer customer = new Customer();
-        //customer.setName(name);
+     public Customer createCustomer(Customer customer){
         customerList.add(customer);
         return customer;
     }
-    
+
     public void removeCustomer(Customer c){
         customerList.remove(c);
     }
-    public boolean checkUsernameIsUnique(String email){
+    
+     public boolean checkUsernameIsUnique(String email){
         for (Customer e : customerList){
             if (e.getEmail().equals(email))
                 return false;
         }
         return true;
     }
+       public Customer updateCustomer(Customer cus, String name, Double height, Double weight, String gender, int age, String address, String city, String zipcode, Boolean none,Boolean diabetes,Boolean cholestrol,Boolean hyper, Boolean pcos, Boolean thyroid){
+        cus.setName(name);
+        cus.setHeight(height);
+        cus.setWeight(weight);
+        cus.setGender(gender);
+        cus.setAge(age);
+        cus.setLocation(address);
+        cus.setCity(city);
+        cus.setZipcode(zipcode);
+        cus.setIs_None(none);
+        cus.setIs_diabetes(diabetes);
+        cus.setIs_thyroid(thyroid);
+        cus.setIs_hypertension(hyper);
+        cus.setIs_pcos(pcos);
+        cus.setIs_cholestrol(cholestrol);
+        
+        return cus;
+    } 
 }
