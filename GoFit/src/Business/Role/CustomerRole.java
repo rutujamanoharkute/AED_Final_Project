21ad7159f874777a6a74ebaf1a0fbd84221d42e9
rutujamanoharkute <kute.r@northeastern.edu>
2022-12-09 12:20:28 -0500

    Created CustomerRole

---
+++ GoFit/src/Business/Role/CustomerRole.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+import Business.Ecosystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import UserInterface.Customer.CustomerWorkAreaPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author rutujakute
+ */
+public class CustomerRole extends Role {
+     @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, Enterprise enterprise, Organization organization, UserAccount account, Ecosystem business, Network network) {
+        return new CustomerWorkAreaPanel(userProcessContainer, enterprise, organization, account, business, network);
+    
+        
+    }
+    
+}
