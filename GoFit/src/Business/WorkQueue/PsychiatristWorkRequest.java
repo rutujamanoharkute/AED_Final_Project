c06cf47dbb2452a19342c3b731a8efa662a4ef58
rutujamanoharkute <kute.r@northeastern.edu>
2022-12-10 14:48:20 -0500

    Created PsychiatristWorkRequest

---
+++ GoFit/src/Business/WorkQueue/PsychiatristWorkRequest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author rutujakute
+ */
+public class PsychiatristWorkRequest extends WorkRequest {
+     private String psychiatristResult;
+
+    public String getPsychiatristResult() {
+        return psychiatristResult;
+    }
+
+    public void setPsychiatristResult(String psychiatristResult) {
+        this.psychiatristResult = psychiatristResult;
+    }
+     
+     
+}
