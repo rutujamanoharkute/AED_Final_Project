3e15923090b235b8ad200119e2465a99012d88dc
rutujamanoharkute <kute.r@northeastern.edu>
2022-12-10 14:49:18 -0500

    Created PersonalCoachWorkRequest

---
+++ GoFit/src/Business/WorkQueue/PersonalCoachWorkRequest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author rutujakute
+ */
+public class PersonalCoachWorkRequest extends WorkRequest{
+    
+     private String personalCoachResult;
+
+    public String getPersonalCoachResult() {
+        return personalCoachResult;
+    }
+
+    public void setPersonalCoachResult(String personalCoachResult) {
+        this.personalCoachResult = personalCoachResult;
+    }
+
+   
+}
