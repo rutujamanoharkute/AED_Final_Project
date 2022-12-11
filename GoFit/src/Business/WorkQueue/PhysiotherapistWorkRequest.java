096f8bdc36808a3127dc2a9092dc40060f31b6dc
rutujamanoharkute <kute.r@northeastern.edu>
2022-12-10 22:59:29 -0500

    Created PhysiotherapistWorkRequest

---
+++ GoFit/src/Business/WorkQueue/PhysiotherapistWorkRequest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author rutujakute
+ */
+public class PhysiotherapistWorkRequest extends WorkRequest{
+    private String physiotherapistResult;
+
+    public String getPhysiotherapistResult() {
+        return physiotherapistResult;
+    }
+
+    public void setPhysiotherapistResult(String physiotherapistResult) {
+        this.physiotherapistResult = physiotherapistResult;
+    }
+    
+}
