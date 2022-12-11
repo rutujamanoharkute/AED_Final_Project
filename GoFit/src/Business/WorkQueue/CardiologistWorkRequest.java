3f3a8a795d88c273d3849789856012a6656c2acd
rutujamanoharkute <kute.r@northeastern.edu>
2022-12-10 22:56:17 -0500

    Created CardiologistWorkRequest

---
+++ GoFit/src/Business/WorkQueue/CardiologistWorkRequest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author rutujakute
+ */
+public class CardiologistWorkRequest extends WorkRequest {
+     private String CardiologistResult;
+
+    public String getCardiologistResult() {
+        return CardiologistResult;
+    }
+
+    public void setCardiologistResult(String CardiologistResult) {
+        this.CardiologistResult = CardiologistResult;
+    }
+     
+    
+}
