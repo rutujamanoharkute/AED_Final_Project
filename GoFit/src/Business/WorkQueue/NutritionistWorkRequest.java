6ed4aeb55c84994cbfdbcb0ad500fc48491e1bc0
rutujamanoharkute <kute.r@northeastern.edu>
2022-12-10 22:57:03 -0500

    Created NutritionistWorkRequest

---
+++ GoFit/src/Business/WorkQueue/NutritionistWorkRequest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author rutujakute
+ */
+public class NutritionistWorkRequest extends WorkRequest {
+    private String dietResult;
+
+    public String getDietResult() {
+        return dietResult;
+    }
+
+    public void setDietResult(String Result) {
+        this.dietResult = Result;
+    }
+}
